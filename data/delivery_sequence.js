385a68b4de9157623c4093fee049ff4f955a07f4
neuhovah <neuhovah@users.noreply.github.com>
2026-02-22 00:10:04 +0100

    Fix delivery sequence variable name

--- data/delivery_sequence.js
+++ data/delivery_sequence.js
@@ -1,4 +1,4 @@
-const stopsData = {"type": "FeatureCollection",
+const delivery_sequenceData = {"type": "FeatureCollection",
 "name": "Delivery Sequence",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
